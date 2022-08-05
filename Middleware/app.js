d13193b1be2b0da391a50b619b3e932bd1b9070e
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-08-05 22:45:42 +0700

    Middleware

---
+++ Middleware/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4001;
+
+const jellybeanBag = {
+  mystery: {
+    number: 4
+  },
+  lemon: {
+    number: 5
+  },
+  rootBeer: {
+    number: 25
+  },
+  cherry: {
+    number: 3
+  },
+  licorice: {
+    number: 1
+  }
+};
+
+// Logging Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} Request Received`);
+  
+});
+
+app.get('/beans/', (req, res, next) => {
+  res.send(jellybeanBag);
+  console.log('Response Sent');
+});
